80f4a7696b20594b0bc354c4ad3689b76d1f68bf
CarloLucibello <carlo.lucibello@gmail.com>
2016-09-16 08:45:51 +0200

    update test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,8 @@
 using SAT
 using Base.Test
 
-E, σ = solve(N=2000, α=9.4, k=4, seed_cnf=19, rstep=0.001, maxiters=1000);
+cnf = randomcnf(N=2000, α=9.4, k=4, seed=19)
+E, σ = solve(cnf, rstep=0.001, maxiters=1000);
 @test E == 0
 
 
